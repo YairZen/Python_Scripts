106af225a06606cf84068a7fa98aef6f910ca8a4
YairZen <zeneve.yair@gmail.com>
2025-10-17 19:43:04 +0300

    added .gitignore & test script

---
+++ Scripts/test_script.py
@@ -0,0 +1,9 @@
+import time
+from datetime import datetime
+
+print("🚀 Starting script...")
+time.sleep(1)
+print(f"Hello {input('Enter your name: ')}!")
+print("Current time:", datetime.now().strftime("%H:%M:%S"))
+time.sleep(1)
+print("✅ Done successfully!")
